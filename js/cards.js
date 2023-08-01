fec3eea15f3aaa60991c4d5474593239b8766155
Ruslan Gumirov <gumirus@gmail.com>
2023-08-01 17:34:48 +0500

    пути ../

--- js/cards.js
+++ js/cards.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const dog = "../img/1.jpeg";
-  const cat = "../img/2.jpeg";
-  const hamster = "../img/3.jpeg";
-  const fish = "../img/4.jpeg";
-  const bird = "../img/5.jpeg";
-  const leopard = "../img/6.jpeg";
+  const dog = "./img/1.jpeg";
+  const cat = "./img/2.jpeg";
+  const hamster = "./img/3.jpeg";
+  const fish = "./img/4.jpeg";
+  const bird = "./img/5.jpeg";
+  const leopard = "./img/6.jpeg";
 
   function createImage(src, alt) {
     const img = document.createElement("img");

394d47f8b3e34553ebbba5693b0fecf39715509b
Ruslan Gumirov <gumirus@gmail.com>
2023-08-01 20:22:20 +0500

    div:moon

--- js/img-cards.js
+++ js/img-cards.js
@@ -5,9 +5,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const fish = "img/fish.jpeg";
   const bird = "img/bird.png";
   const leopard = "img/leopard.jpeg";
-  const luna = "theme/3_luna.png";
-  const clouds = "theme/4_oblako_pered.png";
-  const clouds2 = "theme/2_oblako_zad.png";
+  // const luna = "theme/3_luna.png";
+  // const clouds = "theme/4_oblako_pered.png";
+  // const clouds2 = "theme/2_oblako_zad.png";
 
   function Cards() {
     const container = document.getElementById("cards-container");
@@ -18,9 +18,9 @@ document.addEventListener("DOMContentLoaded", function () {
     container.appendChild(createImage(fish, "fish"));
     container.appendChild(createImage(bird, "bird"));
     container.appendChild(createImage(leopard, "leopard"));
-    container.appendChild(createImage(luna, "luna"));
-    container.appendChild(createImage(clouds, "clouds"));
-    container.appendChild(createImage(clouds2, "clouds2"));
+    // container.appendChild(createImage(luna, "luna"));
+    // container.appendChild(createImage(clouds, "clouds"));
+    // container.appendChild(createImage(clouds2, "clouds2"));
   }
 
   // Call the function to create the cards

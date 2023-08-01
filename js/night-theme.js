d7b2028a5337f2a78057485281d567e6d33e8660
Ruslan Gumirov <gumirus@gmail.com>
2023-08-01 11:28:35 +0500

    fix:cerrections format

--- js/night-theme.js
+++ js/night-theme.js
@@ -1,13 +1,13 @@
-const nightThemeBtn = document.getElementById("night-theme-btn")
+const nightThemeBtn = document.getElementById("night-theme-btn");
 
-nightThemeBtn.addEventListener("click", function() {
-    document.body.classList.toggle("night-theme")
+nightThemeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("night-theme");
 
-    const theme = localStorage.getItem("theme")
+  const theme = localStorage.getItem("theme");
 
-    if (theme === "night-theme") {
-        localStorage.setItem("theme", "")
-    } else {
-        localStorage.setItem("theme", "night-theme")
-    }
-})
+  if (theme === "night-theme") {
+    localStorage.setItem("theme", "");
+  } else {
+    localStorage.setItem("theme", "night-theme");
+  }
+});

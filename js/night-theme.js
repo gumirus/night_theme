6ced0dc1103c11573ff15ffcc130ddad100b4e39
Ruslan Gumirov <gumirus@gmail.com>
2023-08-01 14:08:28 +0500

    add:change text night and day

--- js/night-theme.js
+++ js/night-theme.js
@@ -1,39 +1,35 @@
-// const nightThemeBtn = document.getElementById("night-theme-btn");
-// const moon = document.querySelector(".moon");
-
-// nightThemeBtn.addEventListener("click", function () {
-//   document.body.classList.toggle("night-theme");
-
-//   const theme = localStorage.getItem("theme");
-
-//   if (theme === "night-theme") {
-//     localStorage.setItem("theme", "");
-//     moon.style.opacity = "0"; // Скрываем луну при переключении на светлую тему
-//   } else {
-//     localStorage.setItem("theme", "night-theme");
-//     moon.style.opacity = "1"; // Показываем луну при переключении на темную тему
-//   }
-// });
-
-// const theme = localStorage.getItem("theme");
-// if (theme) {
-//   document.body.classList.add(theme);
-// }
-
 const nightThemeBtn = document.getElementById("night-theme-btn");
 const moon = document.querySelector(".moon");
+const heading = document.getElementById("theme-heading");
+const paragraph = document.getElementById("theme-paragraph");
 
 nightThemeBtn.addEventListener("click", function () {
   document.body.classList.toggle("night-theme");
-  document.body.classList.remove("light-theme"); // Удаляем класс светлой темы при переключении на темную
 
   const theme = localStorage.getItem("theme");
 
   if (theme === "night-theme") {
     localStorage.setItem("theme", "");
     moon.style.opacity = "0"; // Скрываем луну при переключении на светлую тему
+    heading.textContent = "Дневная тема"; // Обновляем текст и заголовок для дневной темы
+    paragraph.textContent = `Дневная тема - это прекрасное время солнца и света.`;
   } else {
     localStorage.setItem("theme", "night-theme");
     moon.style.opacity = "1"; // Показываем луну при переключении на темную тему
+    heading.textContent = "Ночная тема"; // Обновляем текст и заголовок для ночной темы
+    paragraph.textContent = `
+        Ночная тема - это особенное явление, когда природа облачается в мягкие
+        темные тона, а небо становится полотном, украшенным тысячами мерцающих
+        звезд. Она обладает своей уникальной прелестью и неповторимой
+        атмосферой, которые привлекают людей со всего мира. Вот некоторые из
+        аспектов, которые делают ночную тему прекрасной:
+        Звездное небо: Когда солнце заходит за горизонт, наступает время
+        звездного неба. Вдали от светового загрязнения городов, звезды
+        становятся особенно яркими и четко видны на темном небе. Природа
+        удивляет нас своей бесконечной красотой и загадочностью.
+        Луна: Лунный свет придает ночной теме особый шарм. Полная луна создает
+        романтическую атмосферу и освещает окружающий мир деликатными сияющими
+        лучами.
+    `;
   }
 });

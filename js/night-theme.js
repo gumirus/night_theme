821175e8ef06114420ad8518ca5619ce30e9f199
Ruslan Gumirov <gumirus@gmail.com>
2023-08-01 17:58:51 +0500

    unition JS.cards night-theme

--- js/night-theme.js
+++ js/night-theme.js
@@ -3,6 +3,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const heading = document.getElementById("theme-heading");
   const paragraph = document.getElementById("paragraph");
   const theme = localStorage.getItem("theme");
+  const dog = "../img/dog.jpeg";
+  const cat = "../img/cat.jpeg";
+  const hamster = "../img/hamster.jpeg";
+  const fish = "../img/fish.jpeg";
+  const bird = "../img/bird.jpeg";
+  const leopard = "../img/leopard.jpeg";
 
   if (theme === "night-theme") {
     document.body.classList.add("night-theme");
@@ -66,4 +72,25 @@ document.addEventListener("DOMContentLoaded", function () {
       `;
     }
   });
+    
+  function createImage(src, alt) {
+    const img = document.createElement("img");
+    img.src = src;
+    img.alt = alt;
+    return img;
+  }
+
+  function Cards() {
+    const container = document.getElementById("cards-container");
+    // Create and append image elements
+    container.appendChild(createImage(dog, "dog"));
+    container.appendChild(createImage(cat, "cat"));
+    container.appendChild(createImage(hamster, "hamster"));
+    container.appendChild(createImage(fish, "fish"));
+    container.appendChild(createImage(bird, "bird"));
+    container.appendChild(createImage(leopard, "leopard"));
+  }
+
+  // Call the function to create the cards
+  Cards();
 });

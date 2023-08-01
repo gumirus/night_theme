861a03a607cd0d784c8d804f2abe9c8eec723b88
Ruslan Gumirov <gumirus@gmail.com>
2023-08-01 12:55:54 +0500

    add:moon

--- js/night-theme.js
+++ js/night-theme.js
@@ -1,4 +1,5 @@
 const nightThemeBtn = document.getElementById("night-theme-btn");
+const moon = document.querySelector(".moon");
 
 nightThemeBtn.addEventListener("click", function () {
   document.body.classList.toggle("night-theme");
@@ -7,7 +8,9 @@ nightThemeBtn.addEventListener("click", function () {
 
   if (theme === "night-theme") {
     localStorage.setItem("theme", "");
+    moon.style.opacity = "0"; // Скрываем луну при переключении на светлую тему
   } else {
     localStorage.setItem("theme", "night-theme");
+    moon.style.opacity = "1"; // Показываем луну при переключении на темную тему
   }
 });

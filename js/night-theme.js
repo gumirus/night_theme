8210a0cf846d15c40779c7fd76fa67187aa2ead6
Ruslan Gumirov <gumirus29@gmail.com>
2023-08-04 10:11:54 +0500

    add:classes

--- js/night-theme.js
+++ js/night-theme.js
@@ -1,74 +1,68 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const moon = document.querySelector(".moon");
-  const sun = document.querySelector(".sun");
-  const heading = document.getElementById("theme-heading");
-  const paragraph = document.getElementById("paragraph");
-  const theme = localStorage.getItem("theme");
-
-  if (theme === "night-theme") {
-    document.body.classList.add("night-theme");
-    moon.style.display = "block"; // Show the moon icon for night theme
-    sun.style.display = "none"; // Hide the sun icon for night theme
-    heading.textContent = "Ночная тема";
-    paragraph.textContent = `
-      Ночная тема - это особенное явление, когда природа облачается в мягкие
-      темные тона, а небо становится полотном, украшенным тысячами мерцающих
-      звезд. Она обладает своей уникальной прелестью и неповторимой
-      атмосферой, которые привлекают людей со всего мира. Вот некоторые из
-      аспектов, которые делают ночную тему прекрасной:
-      Звездное небо: Когда солнце заходит за горизонт, наступает время
-      звездного неба. Вдали от светового загрязнения городов, звезды
-      становятся особенно яркими и четко видны на темном небе. Природа
-      удивляет нас своей бесконечной красотой и загадочностью.
-      Луна: Лунный свет придает ночной теме особый шарм. Полная луна создает
-      романтическую атмосферу и освещает окружающий мир деликатными сияющими
-      лучами.
-    `;
-  } else {
+class DayTheme {
+  applyTheme() {
     document.body.classList.remove("night-theme");
-    moon.style.display = "none"; // Hide the moon icon for day theme
-    sun.style.display = "block"; // Show the sun icon for day theme
+    moonIcon.hide();
+    sunIcon.show();
     heading.textContent = "Дневная тема";
     paragraph.textContent = `
       Дневная тема - это прекрасное время солнца и света.
     `;
   }
+}
 
-  const nightThemeBtn = document.getElementById("night-theme-btn");
-  nightThemeBtn.addEventListener("click", function () {
-    document.body.classList.toggle("night-theme");
+class NightTheme {
+  applyTheme() {
+    document.body.classList.add("night-theme");
+    moonIcon.show();
+    sunIcon.hide();
+    heading.textContent = "Ночная тема";
+    paragraph.textContent = `
+      Ночная тема - это особенное явление, когда лунный свет придает ночной теме особый шарм. Полная луна создает романтическую атмосферу и освещает окружающий мир деликатными сияющими
+      лучами.
+    `;
+  }
+}
 
-    const theme = document.body.classList.contains("night-theme")
-      ? "night-theme"
-      : "";
+class ElementWithToggle {
+  constructor(selector) {
+    this.element = document.querySelector(selector);
+  }
 
-    localStorage.setItem("theme", theme);
+  show() {
+    this.element.style.display = "block";
+  }
 
-    if (theme === "night-theme") {
-      moon.style.display = "block"; // Show the moon icon when switching to the night theme
-      sun.style.display = "none"; // Hide the sun icon when switching to the night theme
-      heading.textContent = "Ночная тема";
-      paragraph.textContent = `
-        Ночная тема - это особенное явление, когда природа облачается в мягкие
-        темные тона, а небо становится полотном, украшенным тысячами мерцающих
-        звезд. Она обладает своей уникальной прелестью и неповторимой
-        атмосферой, которые привлекают людей со всего мира. Вот некоторые из
-        аспектов, которые делают ночную тему прекрасной:
-        Звездное небо: Когда солнце заходит за горизонт, наступает время
-        звездного неба. Вдали от светового загрязнения городов, звезды
-        становятся особенно яркими и четко видны на темном небе. Природа
-        удивляет нас своей бесконечной красотой и загадочностью.
-        Луна: Лунный свет придает ночной теме особый шарм. Полная луна создает
-        романтическую атмосферу и освещает окружающий мир деликатными сияющими
-        лучами.
-      `;
-    } else {
-      moon.style.display = "none"; // Show the moon icon when switching to the night theme
-      sun.style.display = "block"; // Hide the sun icon when switching to the night theme
-      heading.textContent = "Дневная тема";
-      paragraph.textContent = `
-        Дневная тема - это прекрасное время солнца и света.
-      `;
-    }
-  });
+  hide() {
+    this.element.style.display = "none";
+  }
+}
+
+const dayTheme = new DayTheme();
+const nightTheme = new NightTheme();
+const moonIcon = new ElementWithToggle(".moon");
+const sunIcon = new ElementWithToggle(".sun");
+const heading = document.getElementById("theme-heading");
+const paragraph = document.getElementById("paragraph");
+const theme = localStorage.getItem("theme");
+
+if (theme === "night-theme") {
+  nightTheme.applyTheme();
+} else {
+  dayTheme.applyTheme();
+}
+
+const nightThemeBtn = document.getElementById("night-theme-btn");
+nightThemeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("night-theme");
+
+  const theme = document.body.classList.contains("night-theme")
+    ? "night-theme"
+    : "";
+  localStorage.setItem("theme", theme);
+
+  if (theme === "night-theme") {
+    nightTheme.applyTheme();
+  } else {
+    dayTheme.applyTheme();
+  }
 });

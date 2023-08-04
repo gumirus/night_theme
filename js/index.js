9909e1ef3dc82831a411e0a91813cc54baf62721
Ruslan Gumirov <gumirus@gmail.com>
2023-08-04 14:17:20 +0500

    fix:corrections element JS.

--- js/index.js
+++ js/index.js
@@ -1,79 +1,65 @@
-class DayTheme {
-  applyTheme() {
-    const navbar = document.querySelector(".navbar"); // Get the "navbar" div
-    navbar.classList.remove("night-theme");
-    navbar.classList.add("day-theme");
-    document.body.classList.remove("night-theme");
-    if (moonIcon && sunIcon) {
-      moonIcon.hide();
-      sunIcon.show();
-    }
-    heading.textContent = "Дневная тема";
-    if (paragraph) {
-      paragraph.textContent = `
-        Дневная тема - это прекрасное время солнца и света.
-      `;
-    }
-  }
-}
-
-const dayTheme = new DayTheme();
-
-class NightTheme {
-  applyTheme() {
-    const navbar = document.querySelector(".navbar"); // Get the "navbar" div
-    navbar.classList.remove("day-theme");
-    navbar.classList.add("night-theme");
-    document.body.classList.add("night-theme");
-    if (moonIcon && sunIcon) {
-      moonIcon.show();
-      sunIcon.hide();
-    }
-    heading.textContent = "Ночная тема";
-    if (paragraph) {
-      paragraph.textContent = `
-        Ночная тема - это особенное явление, когда лунный свет придает ночной теме особый шарм. Полная луна создает
-        романтическую атмосферу и освещает окружающий мир деликатными сияющими
-        лучами.
-      `;
-    }
-  }
-}
-
-const nightTheme = new NightTheme();
-
-class ElementWithToggle {
-  constructor(selector) {
-    this.element = document.querySelector(selector);
-  }
-
-  show() {
-    if (this.element) {
-      this.element.style.display = "block";
+(function () {
+  // Classes
+  class DayTheme {
+    applyTheme(heading, paragraph, moonIcon, sunIcon) {
+      const navbar = document.querySelector(".navbar");
+      navbar.classList.remove("night-theme");
+      navbar.classList.add("day-theme");
+      document.body.classList.remove("night-theme");
+      if (moonIcon && sunIcon) {
+        moonIcon.hide();
+        sunIcon.show();
+      }
+      heading.textContent = "Дневная тема";
+      if (paragraph) {
+        paragraph.textContent = `
+          Дневная тема - это прекрасное время солнца и света.
+        `;
+      }
     }
   }
 
-  hide() {
-    if (this.element) {
-      this.element.style.display = "none";
+  class NightTheme {
+    applyTheme(heading, paragraph, moonIcon, sunIcon) {
+      const navbar = document.querySelector(".navbar");
+      navbar.classList.remove("day-theme");
+      navbar.classList.add("night-theme");
+      document.body.classList.add("night-theme");
+      if (moonIcon && sunIcon) {
+        moonIcon.show();
+        sunIcon.hide();
+      }
+      heading.textContent = "Ночная тема";
+      if (paragraph) {
+        paragraph.textContent = `
+          Ночная тема - это особенное явление, когда лунный свет придает ночной теме особый шарм. Полная луна создает
+          романтическую атмосферу и освещает окружающий мир деликатными сияющими
+          лучами.
+        `;
+      }
     }
   }
-}
 
-const moonIcon = new ElementWithToggle(".moon");
-const sunIcon = new ElementWithToggle(".sun");
-const heading = document.getElementById("theme-heading");
-const paragraph = document.getElementById("paragraph");
-const theme = localStorage.getItem("theme");
+  // Element toggle class
+  class ElementWithToggle {
+    constructor(selector) {
+      this.element = document.querySelector(selector);
+    }
 
-if (theme === "night-theme") {
-  nightTheme.applyTheme();
-} else {
-  dayTheme.applyTheme();
-}
+    show() {
+      if (this.element) {
+        this.element.style.display = "block";
+      }
+    }
 
-// Check if the paragraph element exists before manipulating its content
-if (paragraph) {
+    hide() {
+      if (this.element) {
+        this.element.style.display = "none";
+      }
+    }
+  }
+
+  // Theme text contents
   const nightThemeText = `
     Ночная тема - это особенное явление, когда лунный свет придает ночной теме особый шарм. Полная луна создает романтическую атмосферу и освещает окружающий мир деликатными сияющими
     лучами.
@@ -83,31 +69,36 @@ if (paragraph) {
     Дневная тема - это прекрасное время солнца и света.
   `;
 
-  // Apply the appropriate text based on the current theme
-  paragraph.textContent =
-    theme === "night-theme" ? nightThemeText : dayThemeText;
-}
+  // Initialize theme classes and elements
+  const dayTheme = new DayTheme();
+  const nightTheme = new NightTheme();
+  const moonIcon = new ElementWithToggle(".moon");
+  const sunIcon = new ElementWithToggle(".sun");
+  const heading = document.getElementById("theme-heading");
+  const paragraph = document.getElementById("paragraph");
+  const nightThemeBtn = document.getElementById("night-theme-btn");
 
-const nightThemeBtn = document.getElementById("night-theme-btn");
-nightThemeBtn.addEventListener("click", function () {
-  document.body.classList.toggle("night-theme");
-
-  const theme = document.body.classList.contains("night-theme")
-    ? "night-theme"
-    : "";
-  localStorage.setItem("theme", theme);
-
+  // Apply theme based on user preference or default day theme
+  const theme = localStorage.getItem("theme");
   if (theme === "night-theme") {
-    nightTheme.applyTheme();
-    // Update the paragraph text if it exists
-    if (paragraph) {
-      paragraph.textContent = nightThemeText;
-    }
+    nightTheme.applyTheme(heading, paragraph, moonIcon, sunIcon);
   } else {
-    dayTheme.applyTheme();
-    // Update the paragraph text if it exists
-    if (paragraph) {
-      paragraph.textContent = dayThemeText;
-    }
+    dayTheme.applyTheme(heading, paragraph, moonIcon, sunIcon);
   }
-});
+
+  // Event listener for theme toggle button
+  nightThemeBtn.addEventListener("click", function () {
+    document.body.classList.toggle("night-theme");
+    const currentTheme = document.body.classList.contains("night-theme")
+      ? "night-theme"
+      : "";
+
+    localStorage.setItem("theme", currentTheme);
+
+    if (currentTheme === "night-theme") {
+      nightTheme.applyTheme(heading, paragraph, moonIcon, sunIcon);
+    } else {
+      dayTheme.applyTheme(heading, paragraph, moonIcon, sunIcon);
+    }
+  });
+})();
